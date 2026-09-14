bed1be14f33c776adf6079fe3465a7ef5009d0ee
Capybara-Gang148 <kelvinc148@nycstudents.net>
2026-09-14 09:24:35 -0400

    First
    First Turtle

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+t.backward (270)
+square(200)
+t.forward (240)
+t.down (200)
+turtle.done()
